246ba4030f78413341e91141adc3194b0909d6d5
Utkarsh Dixit <utkarshdixit2021@gmail.com>
2022-09-10 21:14:00 +0530

    sixth

--- script.js
+++ script.js
@@ -17,3 +17,48 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
+var a = 0;
+function orderadd0() {
+  a++;
+  document.getElementById("inc0").value = a;
+}
+var b = 0;
+function orderadd1() {
+  b++;
+  document.getElementById("inc1").value = b;
+}
+var c = 0;
+function orderadd2() {
+  c++;
+  document.getElementById("inc2").value = c;
+}
+var d = 0;
+function orderadd3() {
+  d++;
+  document.getElementById("inc3").value = d;
+}
+var e = 0;
+function orderadd4() {
+  e++;
+  document.getElementById("inc4").value = e;
+}
+var f = 0;
+function orderadd5() {
+  f++;
+  document.getElementById("inc5").value = f;
+}
+var g = 0;
+function orderadd6() {
+  g++;
+  document.getElementById("inc6").value = g;
+}
+var h = 0;
+function orderadd7() {
+  h++;
+  document.getElementById("inc7").value = h;
+}
+var i = 0;
+function orderadd8() {
+  i++;
+  document.getElementById("inc8").value = i;
+}
